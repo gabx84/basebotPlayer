8a7ca526fc1fd07924db7bb86537a97ba89c9992
gabx84 <gabx84@gmail.com>
2014-04-09 16:58:47 -0500

    Initial Import.

---
+++ src/com/base/bot/player/BaseBotPlayer.java
@@ -0,0 +1,13 @@
+package com.base.bot.player;
+
+public class BaseBotPlayer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
